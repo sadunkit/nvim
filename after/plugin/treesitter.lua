e96fe03d19487e396e742bc3cdd07154e923b92a
sadunkit <sadun@kityazilim.com>
2024-03-05 21:15:44 +0300

    changed keybinds

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -9,10 +9,8 @@ require'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
-
   highlight = {
-    enable = true,
-
+	enable = true,
     additional_vim_regex_highlighting = false,
   },
 }
--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 require("theprimeagen")
-vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -12,8 +11,21 @@ vim.opt.smartindent = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 
+vim.opt.ignorecase = true -- Make searches case-insensitive
+vim.opt.smartcase = true  -- Search becomes case-sensitive if uppercase letters are used
+
 vim.g.markdown_fenced_languages = {
   "ts=typescript"
 }
 
+-- Copy to clipboard
+vim.keymap.set('v', '<leader>y', '"+y', { noremap = true })
+vim.keymap.set('n', '<leader>Y', '"+yg_', { noremap = true })
+vim.keymap.set('n', '<leader>y', '"+y', { noremap = true })
+vim.keymap.set('n', '<leader>yy', '"+yy', { noremap = true })
 
+-- Paste from clipboard
+vim.keymap.set('n', '<leader>p', '"+p', { noremap = true })
+vim.keymap.set('n', '<leader>P', '"+P', { noremap = true })
+vim.keymap.set('v', '<leader>p', '"+p', { noremap = true })
+vim.keymap.set('v', '<leader>P', '"+P', { noremap = true })
